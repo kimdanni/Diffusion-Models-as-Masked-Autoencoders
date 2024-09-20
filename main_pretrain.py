f3af4e3b539d582453ed6683f2841a0616e5bf54
kimdanni <hyperreals2@gmail.com>
2024-09-20 15:48:04 +0900

    Modify normalization to 0.5 for diffusion-based training

--- main_pretrain.py
+++ main_pretrain.py
@@ -11,123 +11,175 @@
 import argparse
 import datetime
 import json
-import numpy as np
 import os
 import time
 from pathlib import Path
 
-import torch
-import torch.backends.cudnn as cudnn
-from torch.utils.tensorboard import SummaryWriter
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-
+import numpy as np
 import timm
-from timm.data.loader import MultiEpochsDataLoader
 
 # assert timm.__version__ == "0.3.2"  # version check
 import timm.optim.optim_factory as optim_factory
+import torch
+import torch.backends.cudnn as cudnn
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from timm.data.loader import MultiEpochsDataLoader
+from torch.utils.tensorboard import SummaryWriter
 
-import util.misc as misc
-from util.misc import NativeScalerWithGradNormCount as NativeScaler
-from transformer_utils import handle_flash_attn
-
-import models_mae
 import models_cross
-
+import models_mae
+import util.misc as misc
 from engine_pretrain import train_one_epoch
+from transformer_utils import handle_flash_attn
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
 
 
 def get_args_parser():
-    parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
-    parser.add_argument('--batch_size', default=64, type=int,
-                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
-    parser.add_argument('--epochs', default=400, type=int)
-    parser.add_argument('--accum_iter', default=1, type=int,
-                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+    parser = argparse.ArgumentParser("MAE pre-training", add_help=False)
+    parser.add_argument(
+        "--batch_size",
+        default=64,
+        type=int,
+        help="Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus",
+    )
+    parser.add_argument("--epochs", default=400, type=int)
+    parser.add_argument(
+        "--accum_iter",
+        default=1,
+        type=int,
+        help="Accumulate gradient iterations (for increasing the effective batch size under memory constraints)",
+    )
 
     # Model parameters
-    parser.add_argument('--model', default='mae_vit_large_patch16', type=str, metavar='MODEL',
-                        help='Name of model to train')
+    parser.add_argument(
+        "--model",
+        default="mae_vit_large_patch16",
+        type=str,
+        metavar="MODEL",
+        help="Name of model to train",
+    )
 
-    parser.add_argument('--input_size', default=224, type=int,
-                        help='images input size')
+    parser.add_argument("--input_size", default=224, type=int, help="images input size")
 
-    parser.add_argument('--decoder_depth', default=8, type=int, 
-                        help='depth of decoder')
+    parser.add_argument("--decoder_depth", default=8, type=int, help="depth of decoder")
 
-    parser.add_argument('--mask_ratio', default=0.75, type=float,
-                        help='Masking ratio (1 - percentage of remained patches).')
+    parser.add_argument(
+        "--mask_ratio",
+        default=0.75,
+        type=float,
+        help="Masking ratio (1 - percentage of remained patches).",
+    )
 
-    parser.add_argument('--inverse_lr', action='store_true', default=False, help='Use inverse lr scheduler')
-    parser.add_argument('--no_lr_scale', action='store_true', default=False, help='Do not scale lr by mask_ratio')
-    
-    parser.add_argument('--norm_pix_loss', action='store_true',
-                        help='Use (per-patch) normalized pixels as targets for computing loss')
+    parser.add_argument(
+        "--inverse_lr", action="store_true", default=False, help="Use inverse lr scheduler"
+    )
+    parser.add_argument(
+        "--no_lr_scale", action="store_true", default=False, help="Do not scale lr by mask_ratio"
+    )
+
+    parser.add_argument(
+        "--norm_pix_loss",
+        action="store_true",
+        help="Use (per-patch) normalized pixels as targets for computing loss",
+    )
     parser.set_defaults(norm_pix_loss=False)
 
     parser.add_argument(
-        '--find_unused_parameters', action='store_true',
-        help="distributed ddp find unused parameters")
+        "--find_unused_parameters",
+        action="store_true",
+        help="distributed ddp find unused parameters",
+    )
     parser.set_defaults(find_unused_parameters=False)
 
     # Optimizer parameters
-    parser.add_argument('--weight_decay', type=float, default=0.05,
-                        help='weight decay (default: 0.05)')
+    parser.add_argument(
+        "--weight_decay", type=float, default=0.05, help="weight decay (default: 0.05)"
+    )
 
-    parser.add_argument('--lr', type=float, default=None, metavar='LR',
-                        help='learning rate (absolute lr)')
-    parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
-                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
-    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
-                        help='lower lr bound for cyclic schedulers that hit 0')
+    parser.add_argument(
+        "--lr", type=float, default=None, metavar="LR", help="learning rate (absolute lr)"
+    )
+    parser.add_argument(
+        "--blr",
+        type=float,
+        default=1e-3,
+        metavar="LR",
+        help="base learning rate: absolute_lr = base_lr * total_batch_size / 256",
+    )
+    parser.add_argument(
+        "--min_lr",
+        type=float,
+        default=0.0,
+        metavar="LR",
+        help="lower lr bound for cyclic schedulers that hit 0",
+    )
 
-    parser.add_argument('--warmup_epochs', type=int, default=40, metavar='N',
-                        help='epochs to warmup LR')
+    parser.add_argument(
+        "--warmup_epochs", type=int, default=40, metavar="N", help="epochs to warmup LR"
+    )
 
     # Dataset parameters
-    parser.add_argument('--data_path', default='/datasets01/imagenet_full_size/061417/', type=str,
-                        help='dataset path')
+    parser.add_argument(
+        "--data_path",
+        default="/datasets01/imagenet_full_size/061417/",
+        type=str,
+        help="dataset path",
+    )
 
-    parser.add_argument('--output_dir', default='./output_dir',
-                        help='path where to save, empty for no saving')
-    parser.add_argument('--log_dir', default=None,
-                        help='path where to tensorboard log')
-    parser.add_argument('--device', default='cuda',
-                        help='device to use for training / testing')
-    parser.add_argument('--seed', default=0, type=int)
-    parser.add_argument('--resume', default='',
-                        help='resume from checkpoint')
+    parser.add_argument(
+        "--output_dir", default="./output_dir", help="path where to save, empty for no saving"
+    )
+    parser.add_argument("--log_dir", default=None, help="path where to tensorboard log")
+    parser.add_argument("--device", default="cuda", help="device to use for training / testing")
+    parser.add_argument("--seed", default=0, type=int)
+    parser.add_argument("--resume", default="", help="resume from checkpoint")
 
-    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
-                        help='start epoch')
-    parser.add_argument('--num_workers', default=10, type=int)
-    parser.add_argument('--pin_mem', action='store_true',
-                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
-    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.add_argument("--start_epoch", default=0, type=int, metavar="N", help="start epoch")
+    parser.add_argument("--num_workers", default=10, type=int)
+    parser.add_argument(
+        "--pin_mem",
+        action="store_true",
+        help="Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.",
+    )
+    parser.add_argument("--no_pin_mem", action="store_false", dest="pin_mem")
     parser.set_defaults(pin_mem=True)
-    
-    parser.add_argument('--multi_epochs_dataloader', action='store_true', help='Use MultiEpochsDataLoader to prevent reinitializing dataloader per epoch')
+
+    parser.add_argument(
+        "--multi_epochs_dataloader",
+        action="store_true",
+        help="Use MultiEpochsDataLoader to prevent reinitializing dataloader per epoch",
+    )
 
     # distributed training parameters
-    parser.add_argument('--world_size', default=1, type=int,
-                        help='number of distributed processes')
-    parser.add_argument('--local_rank', default=-1, type=int)
-    parser.add_argument('--dist_on_itp', action='store_true')
-    parser.add_argument('--dist_url', default='env://',
-                        help='url used to set up distributed training')
+    parser.add_argument("--world_size", default=1, type=int, help="number of distributed processes")
+    parser.add_argument("--local_rank", default=-1, type=int)
+    parser.add_argument("--dist_on_itp", action="store_true")
+    parser.add_argument(
+        "--dist_url", default="env://", help="url used to set up distributed training"
+    )
 
     # MAE or cross-MAE
-    parser.add_argument('--cross_mae', action='store_true', default=False)
-    parser.add_argument('--weight_fm', action='store_true', default=False,
-                        help='Weight the feature maps for decoder when running cross-mae')
-    parser.add_argument('--use_fm', nargs='+', type=int, default=[-1], 
-                        help='Feature maps to use for decoder')
-    parser.add_argument('--use_input', action='store_true', default=False,
-                        help="use input as a feature map")
-    parser.add_argument('--self_attn', action='store_true', default=False, help="use self attention in decoder")
-    
-    parser.add_argument('--enable_flash_attention2', action='store_true', default=False, help="Use flash attntion 2")
+    parser.add_argument("--cross_mae", action="store_true", default=False)
+    parser.add_argument(
+        "--weight_fm",
+        action="store_true",
+        default=False,
+        help="Weight the feature maps for decoder when running cross-mae",
+    )
+    parser.add_argument(
+        "--use_fm", nargs="+", type=int, default=[-1], help="Feature maps to use for decoder"
+    )
+    parser.add_argument(
+        "--use_input", action="store_true", default=False, help="use input as a feature map"
+    )
+    parser.add_argument(
+        "--self_attn", action="store_true", default=False, help="use self attention in decoder"
+    )
+
+    parser.add_argument(
+        "--enable_flash_attention2", action="store_true", default=False, help="Use flash attntion 2"
+    )
 
     return parser
 
@@ -135,8 +187,8 @@ def get_args_parser():
 def main(args):
     misc.init_distributed_mode(args)
 
-    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
-    print("{}".format(args).replace(', ', ',\n'))
+    print(f"job dir: {os.path.dirname(os.path.realpath(__file__))}")
+    print(f"{args}".replace(", ", ",\n"))
 
     device = torch.device(args.device)
 
@@ -146,16 +198,23 @@ def main(args):
     np.random.seed(seed)
 
     cudnn.benchmark = True
-    
+
     handle_flash_attn(args)
 
     # simple augmentation
-    transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(args.input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomResizedCrop(
+                args.input_size, scale=(0.2, 1.0), interpolation=3
+            ),  # 3 is bicubic
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-    dataset_train = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    )
+    dataset_train = datasets.ImageFolder(
+        os.path.join(args.data_path, "train"), transform=transform_train
+    )
     print(dataset_train)
 
     if True:  # args.distributed:
@@ -174,22 +233,25 @@ def main(args):
     else:
         log_writer = None
 
-    dataloader_cls = MultiEpochsDataLoader if args.multi_epochs_dataloader else torch.utils.data.DataLoader
+    dataloader_cls = (
+        MultiEpochsDataLoader if args.multi_epochs_dataloader else torch.utils.data.DataLoader
+    )
 
     data_loader_train = dataloader_cls(
-        dataset_train, sampler=sampler_train,
+        dataset_train,
+        sampler=sampler_train,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
         pin_memory=args.pin_mem,
         drop_last=True,
     )
-    
+
     # define the model
     if args.cross_mae:
         model = models_cross.__dict__[args.model](
-            norm_pix_loss=args.norm_pix_loss, 
-            weight_fm=args.weight_fm, 
-            decoder_depth=args.decoder_depth, 
+            norm_pix_loss=args.norm_pix_loss,
+            weight_fm=args.weight_fm,
+            decoder_depth=args.decoder_depth,
             use_fm=args.use_fm,
             use_input=args.use_input,
             self_attn=args.self_attn,
@@ -197,7 +259,7 @@ def main(args):
         )
     else:
         model = models_mae.__dict__[args.model](
-            norm_pix_loss=args.norm_pix_loss, 
+            norm_pix_loss=args.norm_pix_loss,
             decoder_depth=args.decoder_depth,
         )
 
@@ -219,18 +281,19 @@ def main(args):
 
     if args.distributed:
         model = torch.nn.parallel.DistributedDataParallel(
-            model, device_ids=[args.gpu], 
-            find_unused_parameters=args.find_unused_parameters
+            model, device_ids=[args.gpu], find_unused_parameters=args.find_unused_parameters
         )
         model_without_ddp = model.module
-    
+
     # following timm: set wd as 0 for bias and norm layers
     param_groups = optim_factory.param_groups_weight_decay(model_without_ddp, args.weight_decay)
     optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
     print(optimizer)
     loss_scaler = NativeScaler()
 
-    misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
+    misc.load_model(
+        args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler
+    )
 
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
@@ -238,27 +301,51 @@ def main(args):
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
         train_stats = train_one_epoch(
-            model, data_loader_train,
-            optimizer, device, epoch, loss_scaler,
+            model,
+            data_loader_train,
+            optimizer,
+            device,
+            epoch,
+            loss_scaler,
             log_writer=log_writer,
-            args=args
+            args=args,
         )
         if args.output_dir:
             if epoch % 200 == 0:
                 misc.save_model(
-                    args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                    loss_scaler=loss_scaler, epoch=epoch, save_latest_model_only=False)
+                    args=args,
+                    model=model,
+                    model_without_ddp=model_without_ddp,
+                    optimizer=optimizer,
+                    loss_scaler=loss_scaler,
+                    epoch=epoch,
+                    save_latest_model_only=False,
+                )
             elif epoch % 20 == 0:
                 misc.save_model(
-                    args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                    loss_scaler=loss_scaler, epoch=epoch, save_latest_model_only=True)
+                    args=args,
+                    model=model,
+                    model_without_ddp=model_without_ddp,
+                    optimizer=optimizer,
+                    loss_scaler=loss_scaler,
+                    epoch=epoch,
+                    save_latest_model_only=True,
+                )
             if epoch + 1 == args.epochs:
                 misc.save_model(
-                    args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                    loss_scaler=loss_scaler, epoch=epoch, save_latest_model_only=True)
+                    args=args,
+                    model=model,
+                    model_without_ddp=model_without_ddp,
+                    optimizer=optimizer,
+                    loss_scaler=loss_scaler,
+                    epoch=epoch,
+                    save_latest_model_only=True,
+                )
 
-        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
-                        'epoch': epoch,}
+        log_stats = {
+            **{f"train_{k}": v for k, v in train_stats.items()},
+            "epoch": epoch,
+        }
 
         if args.output_dir and misc.is_main_process():
             if log_writer is not None:
@@ -268,10 +355,10 @@ def main(args):
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print('Training time {}'.format(total_time_str))
+    print(f"Training time {total_time_str}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args_parser()
     args = args.parse_args()
     if args.log_dir is None:
